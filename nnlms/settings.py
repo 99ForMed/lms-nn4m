8e67dbfbd0f80444b610f24d5499a97dfa416dce
callmepri2003 <48148425+callmepri2003@users.noreply.github.com>
2023-02-15 06:09:37 +1100

    ADS

--- nnlms/settings.py
+++ nnlms/settings.py
@@ -120,6 +120,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
