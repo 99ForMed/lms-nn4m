fadd6aac470057422aaed45661d4e66966e7f66e
callmepri2003 <48148425+callmepri2003@users.noreply.github.com>
2023-02-17 14:35:36 +1100

    gdhb

--- general/models.py
+++ general/models.py
@@ -32,6 +32,7 @@ class UcatVideo(models.Model):
     description = models.TextField()
     thumbnail = models.ImageField(null = True)
     url = models.URLField()
+    testing = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
